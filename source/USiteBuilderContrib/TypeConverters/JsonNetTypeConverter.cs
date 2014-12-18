17494c79ca94a0d178b0246b199334a48aa122ea
Boyan Mihaylov <bm@ebita.dk>
2014-12-18 15:54:27 +0100

    convert from/to json.net; render Grid extension from object model;

---
+++ source/USiteBuilderContrib/TypeConverters/JsonNetTypeConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Newtonsoft.Json;
+using Vega.USiteBuilder.Types;
+
+namespace USiteBuilderContrib.TypeConverters
+{
+    public class JsonNetTypeConverter : ICustomTypeConvertor
+    {
+        public Type ConvertType
+        {
+            get { return typeof(JsonNetTypeConverter); }
+        }
+
+        public object ConvertValueWhenRead(object inputValue)
+        {
+            return inputValue != null ? JsonConvert.DeserializeObject(inputValue.ToString()) : null;
+        }
+
+        public object ConvertValueWhenWrite(object inputValue)
+        {
+            return JsonConvert.SerializeObject(inputValue);
+        }
+    }
+}
---
+++ source/USiteBuilderContrib/Umbraco/GridTemplateExtensions.cs
@@ -0,0 +1,45 @@
+﻿using System.IO;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using Umbraco.Web;
+
+namespace USiteBuilderContrib.Umbraco
+{
+    public static class GridTemplateExtensions
+    {
+        public static IHtmlString RenderGrid(this UmbracoHelper helper, object propertyValue, string framework = "bootstrap3")
+        {
+            var view = "Grid/" + framework;
+            return new HtmlString(RenderPartialViewToString(view, propertyValue));
+        }
+
+        private static string RenderPartialViewToString(string viewName, object model)
+        {
+            using (var sw = new StringWriter())
+            {
+                HttpRequestBase httpRequest = UmbracoContext.Current.HttpContext.Request;
+                var cc = new ControllerContext
+                {
+                    RequestContext = new RequestContext(UmbracoContext.Current.HttpContext,
+                        new RouteData { Route = RouteTable.Routes["Umbraco_default"] })
+                };
+                cc.RequestContext.RouteData.Values.Add("action", httpRequest.RequestContext.RouteData.Values["action"]);
+                cc.RequestContext.RouteData.Values.Add("controller", httpRequest.RequestContext.RouteData.Values["controller"]);
+
+                var partialView = ViewEngines.Engines.FindPartialView(cc, viewName);
+                var tempData = new TempDataDictionary();
+                var viewData = new ViewDataDictionary
+                {
+                    Model = model
+                };
+
+                var innerViewContext = new ViewContext(cc, partialView.View, viewData, tempData, sw);
+                partialView.View.Render(innerViewContext, sw);
+                partialView.ViewEngine.ReleaseView(cc, partialView.View);
+
+                return sw.GetStringBuilder().ToString();
+            }
+        }
+    }
+}
